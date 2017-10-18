6fd5234a364ca7c00ce3358e2e6ede7d6bb9e5df
ruanguihua <ruanguihua@ccw163.com>
2017-10-18 21:15:19 +0800

    更新

--- app/src/main/java/com/rgh/multiplervview/MainActivity.java
+++ app/src/main/java/com/rgh/multiplervview/MainActivity.java
@@ -2,12 +2,16 @@ package com.rgh.multiplervview;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView mRecyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mRecyclerView = (RecyclerView) findViewById(R.id.rv);
     }
 }
